d9d97420ce8cdc949dad68aa9d3d36eddee9e24a
Alan Syrgak <96595532+Syrgak-Alan@users.noreply.github.com>
2024-02-15 10:09:53 +0100

    Update DynamicContentSubscriber.php (#3)
    * Update DynamicContentSubscriber.php
    
    * all errrors are solved and modified option classifier
    
    * done subtasks 5029, 5030, 5031
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md

--- EventListener/DynamicContentSubscriber.php
+++ EventListener/DynamicContentSubscriber.php
@@ -4,14 +4,13 @@ namespace MauticPlugin\LeuchtfeuerDwcDeviceTypeBundle\EventListener;
 
 use Mautic\DynamicContentBundle\DynamicContentEvents;
 use Mautic\DynamicContentBundle\Event\ContactFiltersEvaluateEvent;
-use Mautic\LeadBundle\Entity\LeadDevice;
 use Mautic\LeadBundle\Model\DeviceModel;
-use MauticPlugin\LeuchtfeuerDwcDeviceTypeBundle\Services\DynamicContentService;
+use Mautic\PluginBundle\Helper\IntegrationHelper;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
 class DynamicContentSubscriber implements EventSubscriberInterface
 {
-    public function __construct(protected DeviceModel $deviceModel, protected DynamicContentService $dynamicContentService)
+    public function __construct(private IntegrationHelper $helper, private DeviceModel $deviceModel)
     {
     }
 
@@ -24,20 +23,52 @@ class DynamicContentSubscriber implements EventSubscriberInterface
 
     public function onContactFiltersEvaluate(ContactFiltersEvaluateEvent $event): void
     {
+        $myIntegration = $this->helper->getIntegrationObject('LeuchtfeuerDwcDeviceType');
+
+        if (false === $myIntegration || !$myIntegration->getIntegrationSettings()->getIsPublished()) {
+            return;
+        }
+
         $filters = $event->getFilters();
         $contact = $event->getContact();
-        /** @var LeadDevice $leadDevice */
-        $leadDevice = $this->deviceModel->getEntity($contact->getId());
+        $leadDeviceRepository = $this->deviceModel->getRepository();
+        $leadDevice = $leadDeviceRepository->getLeadDevices($contact);
         if (empty($leadDevice)) {
             return;
         }
 
-        $deviceType = $leadDevice->getDevice();
+        $deviceType = $leadDevice[0]['device'];
         foreach ($filters as $filter) {
             if ('device_type' === $filter['type']) {
-                if ($deviceType) {
-                    $event->setIsEvaluated(true);
-                    $event->setIsMatched(in_array($deviceType, $filter['filter']));
+                switch ($filter['operator']) {
+                    case 'in':
+                        if (in_array($deviceType, $filter['filter'])) {
+                            $event->setIsEvaluated(true);
+                            $event->setIsMatched(in_array($deviceType, $filter['filter']));
+                        }
+                        break;
+                    case '!in':
+                        if (!in_array($deviceType, $filter['filter'])) {
+                            $event->setIsEvaluated(true);
+                            $event->setIsMatched(!in_array($deviceType, $filter['filter']));
+                        }
+                        break;
+                    case 'empty':
+                        if (empty($deviceType)) {
+                            $event->setIsEvaluated(true);
+                            $event->setIsMatched(empty($deviceType));
+                        }
+                        break;
+                    case '!empty':
+                        if (!empty($deviceType)) {
+                            $event->setIsEvaluated(true);
+                            $event->setIsMatched(!empty($deviceType));
+                        }
+                        break;
+                    default:
+                        $event->setIsEvaluated(true);
+                        $event->setIsMatched(false);
+                        break;
                 }
             }
         }
--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# mautic-DwcDeviceType-bundle
-Adds the option to filter for device type in dynamic web content.
+# DWC Filter Device Type
+
+### Adds the option to filter for device type in dynamic web content.
+
+## Setting up the right requirements 
+
+- Mautic 5.0.0 or higher
+- PHP 8.0 or higher
+- Before installing plugin, please make sure to install changes from PR https://github.com/mautic/mautic/pull/13226, otherwise plugin will not work
+
+## Installation
+- Plugin has to be saved within plugins/LeuchtfeuerDwcDeviceTypeBundle/
+- After completed installing of plugin, the cache has to be cleared( php bin/console c:c )
+- Reload plugins page and press "Install/Upgrade Plugins" button
+- Now plugin appeared, open it and publish by switching to button "Yes" under "Published" label and save configuration
+
+## How to use this plugin?
+- After enabling plugin, create new Dynamic Content. 
+- Make this DWC not campaign based on the right-hand side *->* give him **slot_name** and fill content. 
+- Now go to filters section, which appeared after setting this DWC to not campaign based and add filter "Device Type" where you can select operator(including, excluding, empty, not empty) and choose which device types you want to be filtered
+- Save and close
+- Go to Landing Pages *->* select already existing page or create new and add in builder DWC by writing {dwc=**slot_name**}
+- Save and exit
+
+## Author
+Leuchtfeuer Digital Marketing GmbH
+
+mautic@Leuchtfeuer.com
