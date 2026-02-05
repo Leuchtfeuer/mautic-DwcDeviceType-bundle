9cb30ceb639681c01ab22e474637e7d7ffbfc9b6
Ekkehard Gümbel <43146234+ekkeguembel@users.noreply.github.com>
2026-02-05 17:36:31 +0100

    Update config.php

--- Config/config.php
+++ Config/config.php
@@ -3,7 +3,7 @@
 return [
     'name'          => 'Device type filter for DWC',
     'description'   => 'Introduce menu item which gives access to the Audit Log',
-    'version'       => '1.1.1',
+    'version'       => '5.0.0',
     'author'        => 'Leuchtfeuer Digital Marketing GmbH',
 
     'routes'        => [],
